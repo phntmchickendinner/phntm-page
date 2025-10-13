839145cd5842b819a94787e0f6b4139e0fb7b033
phntmchickendinner <luqm4nulh4kim000@gmail.com>
2025-10-13 12:13:07 +0800

    Update templatemo-neural-scripts.js

--- docs/templatemo-neural-scripts.js
+++ docs/templatemo-neural-scripts.js
@@ -183,10 +183,11 @@ https://templatemo.com/tm-597-neural-glass
                 this.style.background = 'linear-gradient(45deg, #00ff00, #00ffff)';
                 
                 setTimeout(() => {
-                    this.innerHTML = 'DIHANTAR KE AKIMM (BETA)';
+                    this.innerHTML = 'DIHANTAR KE AKIMM';
                     this.style.background = 'linear-gradient(45deg, #00ffff, #ff0080)';
                 }, 2000);
             }, 1500);
 
         });
+
 
